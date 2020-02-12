b37d85e1d00559d9ba61c4a306bb27b552557f75
JamesGood626 <jamesgood626@gmail.com>
2020-02-12 15:14:24 -0500

    saving progress

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import client from "./graphQL/client";
 import Main from "./Main";
 import "./reset.css";
 
+// Remember to run all tests in order to execute the tests.
+// Also, it'd be fucking great if you'd just do a write up on how
+// you set the testing up for this shiz.
 class App extends Component {
   render() {
     return (
